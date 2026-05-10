fe3133a1b71a1f251bc0c9d9063be9801ba68ce6
man-of-culturee <amanuelashu86@gmail.com>
2026-05-10 15:55:51 +0300

    feat: added a file adding functionality

--- ftd.py
+++ ftd.py
@@ -1,4 +1,13 @@
 import tkinter as tk
+from tkinter import filedialog
+
+
+# Add file
+def add_file():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        file_listbox.insert(tk.END, file_path)
+
 
 # window
 root = tk.Tk()
@@ -17,5 +26,14 @@ frame.pack()
 file_listbox = tk.Listbox(frame, width=70, height=10)
 file_listbox.grid(row=0, column=0, columnspan=2, pady=10)
 
+# Buttons frame
+btn_frame = tk.Frame(root)
+btn_frame.pack(pady=5)
+
+btn_style = {"width": 18, "height": 2}
+
+tk.Button(btn_frame, text="Add File", command=add_file, **btn_style).grid(
+    row=0, column=0, padx=5
+)
 
 root.mainloop()
